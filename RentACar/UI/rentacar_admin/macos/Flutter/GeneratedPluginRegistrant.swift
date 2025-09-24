720fadeb74c864d8fb4354092754d8f0b1430c17
Dženan Magrdžija <dzenan.magrdzija@edu.fit.ba>
2025-09-24 17:57:52 +0200

    .

--- RentACar/UI/rentacar_admin/macos/Flutter/GeneratedPluginRegistrant.swift
+++ RentACar/UI/rentacar_admin/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import file_picker
+import open_file_mac
 import path_provider_foundation
 import printing
 import screen_retriever
@@ -12,6 +14,8 @@ import share_plus
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
+  OpenFilePlugin.register(with: registry.registrar(forPlugin: "OpenFilePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
   ScreenRetrieverPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverPlugin"))
